e2ec643466ed6ec0740ec65c608494cc3945fa3c
AlyonaSlayer <forworkinlife@gmail.com>
2024-05-02 17:12:55 +0300

    Update lesson_2_task_10.py

--- lesson_2/lesson_2_task_10.py
+++ lesson_2/lesson_2_task_10.py
@@ -1,7 +1,7 @@
 def bank():
     x = float(input("Введите сумму вклада "))
     y = int(input ("Введите срок хранения "))
-    for i in range(y):
+    for i in range(y+1):
         x = x+x/10
         print(i, x)
 bank()
