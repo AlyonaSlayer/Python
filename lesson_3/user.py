82d661cfe66fe38c14ba28ae46c9c78a7f3c459c
AlyonaSlayer <forworkinlife@gmail.com>
2024-05-04 18:59:27 +0300

    Update user.py

--- lesson_3/user.py
+++ lesson_3/user.py
@@ -1,14 +1,14 @@
 class User:
     
     def __init__(self, first_name, last_name):
-        self.name = first_name
-        self.surname = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         
     def sayName(self):
-        print(self.name)
+        print(self.first_name)
 
     def saySurname(self):
-        print(self.surname)
+        print(self.last_name)
     
     def sayFNLN(self):
-        print(self.name, self.surname)
+        print(self.first_name, self.last_name)
