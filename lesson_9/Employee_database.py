763cf4b7b0b8c92854f6f7ac288260659ae5ecdd
AlyonaSlayer <forworkinlife@gmail.com>
2024-06-21 20:57:28 +0300

    1

---
+++ lesson_9/Employee_database.py
@@ -0,0 +1,3 @@
+from sqlalchemy import create_engine
+
+db_connection_string = "postgres://x_clients_db_3fmx_user:mzoTw2Vp4Ox4NQH0XKN3KumdyAYE31uq@dpg-cour99g21fec73bsgvug-a.oregon-postgres.render.com/x_clients_db_3fmx"
---
+++ lesson_9/test_x_client_db.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+
+db_connection_string = "postgresql://x_clients_db_3fmx_user:mzoTw2Vp4Ox4NQH0XKN3KumdyAYE31uq@dpg-cour99g21fec73bsgvug-a.oregon-postgres.render.com/x_clients_db_3fmx"
+
+def test_dasd():
+    ad = 1+1
+    assert ad == 2
